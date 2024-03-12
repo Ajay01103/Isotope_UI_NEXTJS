2541f5af447d0ed3e0947c94faafa893a3443eca
Ajay01103 <125455606+Ajay01103@users.noreply.github.com>
2024-03-12 15:31:02 +0530

    initial commit

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,6 +12,7 @@ import {
 import React from "react"
 import Link from "next/link"
 import { Button } from "./ui/button"
+import { MobileNavbar } from "./mobile-navbar"
 
 export const Navbar = () => {
   const components: { title: string; href: string; description: string }[] = [
@@ -53,12 +54,9 @@ export const Navbar = () => {
 
   return (
     <>
-      <header className="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm py-3 md:py-6 mb-8">
-        <nav
-          className="max-w-[85rem] w-full mx-auto px-4 md:px-6 lg:px-8"
-          aria-label="Global"
-        >
-          <div className="relative md:flex md:items-center md:justify-between">
+      <header className="sticky top-0 z-50 w-full border-b bg-background text-sm py-3 md:py-6 mb-8">
+        <nav className="md:max-w-[85rem] flex justify-between w-full mx-auto px-4 md:px-6 lg:px-8">
+          <div className="relative flex w-full items-center justify-between">
             <div className="flex items-center justify-between">
               <a
                 className="flex-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
@@ -98,66 +96,10 @@ export const Navbar = () => {
                   />
                 </svg>
               </a>
-
-              <div className="md:hidden">
-                <button
-                  type="button"
-                  className="hs-collapse-toggle size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                  data-hs-collapse="#navbar-collapse-with-animation"
-                  aria-controls="navbar-collapse-with-animation"
-                  aria-label="Toggle navigation"
-                >
-                  <svg
-                    className="hs-collapse-open:hidden flex-shrink-0 size-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <line
-                      x1="3"
-                      x2="21"
-                      y1="6"
-                      y2="6"
-                    />
-                    <line
-                      x1="3"
-                      x2="21"
-                      y1="12"
-                      y2="12"
-                    />
-                    <line
-                      x1="3"
-                      x2="21"
-                      y1="18"
-                      y2="18"
-                    />
-                  </svg>
-                  <svg
-                    className="hs-collapse-open:block hidden flex-shrink-0 size-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M18 6 6 18" />
-                    <path d="m6 6 12 12" />
-                  </svg>
-                </button>
-              </div>
+              {/* <MobileNavbar side="right" /> */}
             </div>
 
-            <NavigationMenu>
+            <NavigationMenu className="ml-4 hidden lg:flex">
               <NavigationMenuList>
                 <NavigationMenuItem>
                   <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
@@ -488,6 +430,7 @@ export const Navbar = () => {
               </div>
             </div>
           </div>
+          <MobileNavbar />
         </nav>
       </header>
     </>
